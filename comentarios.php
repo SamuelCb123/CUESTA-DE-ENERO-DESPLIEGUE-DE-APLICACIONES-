bf77e03ef36077bff93c798d7a7b37def769f2d4
Samuel Castro <dreift133@gmail.com>
2024-01-10 23:48:01 +0100

    acabar mañana

--- comentarios.php
+++ comentarios.php
@@ -10,59 +10,56 @@
 </head>
 <body>
 <header>
-        <nav>
-            <img src="logo.PNG" width="100px">
-            <ul>
-                <li><a href="index.html">Inicio</a></li>
-                <li><a href="comentarios.php"> Opiniones</a></li>
-            </ul>
-        </nav>
-        </header>
+    <nav>
+        <img src="logo.PNG" width="100px">
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="comentarios.php"> Opiniones</a></li>
+        </ul>
+    </nav>
+</header>
 
-
-    <form method="POST" action="enviarcomentario.php">
-        <section id="contact">
-            <div class="container px-4">
-                <div class="row gx-4 justify-content-center">
-                    <div class="col-lg-8">
-                        <h2>Valoraciones</h2>
+<form method="POST" action="enviarcomentario.php">
+    <section id="contact">
+        <div class="container px-4">
+            <div class="row gx-4 justify-content-center">
+                <div class="col-lg-8">
+                    <h2>Valoraciones</h2>
+                    <br>
+                    <p class="lead">Introduce tu Experiencia </p>
+                    <br>
+                    <div class="col-xs-12">
+                        <h3>Realiza un comentario </h3>
                         <br>
-                        <p class="lead">Introduce tu Experiencia </p>
-                        <br>
-                        <div class="col-xs-12">
-                            <h3>Realiza un comentario </h3>
-                            <br>
-                            <div class="form-group">
-                                <label for="nombre" class="form-label">Nombre</label>
-                                <input class="form-control" name="nombre" type="text" id="nombre" placeholder="Escribe tu nombre">
-                            </div>
-                            <div class="form-group">
-                                <label for="comentario" class="form-label">Comentario</label>
-                                <textarea class="form-control" name="comentario" cols="30" row="5" type="text" id="comentario" placeholder="Escribe tu comentario..."></textarea>
-                            </div>
-                            <br>
-                            <input class="btn btn-primary" type="submit" value="Enviar comentario">
-                            <br>
-                            <br>
-                            <br>
-                            <?php
-                            include('conexion.php');
-                            $resultado = mysqli_query($conexion, 'SELECT * FROM comentarios');
-                            while ($comentario = mysqli_fetch_object($resultado)) {
-                                echo '<b>' . $comentario->nombre . '</b> (' . $comentario->fecha . ') dijo:';
-                                echo '<br/>';
-                                echo $comentario->comentario;
-                                echo '<br/>';
-                            }
-                            ?>
+                        <div class="form-group">
+                            <label for="nombre" class="form-label">Nombre</label>
+                            <input class="form-control" name="nombre" type="text" id="nombre" placeholder="Escribe tu nombre">
                         </div>
+                        <div class="form-group">
+                            <label for="comentario" class="form-label">Comentario</label>
+                            <textarea class="form-control" name="comentario" cols="30" rows="5" id="comentario" placeholder="Escribe tu comentario..."></textarea>
+                        </div>
+                        <br>
+                        <input class="btn btn-primary" type="submit" value="Enviar comentario">
+                        <br>
+                        <br>
+                        <br>
+                        <?php
+                        include('conexion.php');
+                        $resultado = mysqli_query($conexion, 'SELECT * FROM comentarios');
+                        while ($comentario = mysqli_fetch_object($resultado)) {
+                            echo '<b>' . $comentario->nombre . '</b> (' . $comentario->fecha . ') dijo:';
+                            echo '<br/>';
+                            echo $comentario->comentario;
+                            echo '<br/>';
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
-        </section>
-    </form>
-    <footer>
-    <p>&copy; 2024 Samuel</p>
-</footer>
+        </div>
+    </section>
+</form>
+
 </body>
 </html>
