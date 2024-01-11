a6e8b1dd53aea9f076134c06618e97817407ed5c
Samuel Castro <dreift133@gmail.com>
2024-01-11 20:14:01 +0100

    ..

--- enviarcomentario.php
+++ enviarcomentario.php
@@ -3,9 +3,6 @@ include('conexion.php');
 $nombre = $_POST['nombre'];
 $comentario = $_POST['comentario'];
 
-$nombre = mysqli_real_escape_string($conexion, $nombre);
-$comentario = mysqli_real_escape_string($conexion, $comentario);
-
 
 $resultado = mysqli_query($conexion, 'INSERT INTO comentarios (nombre, comentario) VALUES ("' . $nombre . '", "' . $comentario . '")');
 header('Location:comentarios.php');
